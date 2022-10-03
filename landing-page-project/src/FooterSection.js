e12b75d1640ad1a275626904beb4edc515913f1c
Katarina <katarina.floyd1@gmail.com>
2022-10-03 16:22:07 +0200

    add footer section and refactor responsiveness

--- landing-page-project/src/FooterSection.js
+++ landing-page-project/src/FooterSection.js
@@ -1,7 +1,11 @@
 export default function FooterSection() {
   return (
-    <div>
-      <h2>Footer Section</h2>
+    <div className="footer-section">
+        <h1>SUBSCRIBE TO OUR EMAIL LIST</h1>
+        <form>
+          <input className='email-input' type='email' placeholder="What's your email?" />
+          <input className='submit-btn' type='button' value='Submit' />
+        </form>
     </div>
   )
 }
