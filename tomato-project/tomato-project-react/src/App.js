55f687f2f8c217cc79849fd56739c37f7c61b9a7
Katarina <katarina.floyd1@gmail.com>
2022-10-06 10:16:38 +0200

    clean code and add basic structure

--- tomato-project/tomato-project-react/src/App.js
+++ tomato-project/tomato-project-react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class-Name="container">
+        <h1>Tomato Counter</h1>
+        <div className="tomato-container">
+          <div className="btn-container">
+            <button>+</button>
+            <button>-</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
