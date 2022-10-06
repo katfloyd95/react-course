e97afae58a47625307ddcb7292d78beeef3c6db5
Katarina <katarina.floyd1@gmail.com>
2022-10-06 11:03:23 +0200

    add tomato counter number functionality

--- tomato-project/tomato-project-react/src/App.js
+++ tomato-project/tomato-project-react/src/App.js
@@ -1,14 +1,33 @@
+import React from 'react';
+
 function App() {
+
+  const [number, setNumber] = React.useState(0);
+
+  function addNumber() {
+    return (
+      setNumber(number + 1)
+    )
+  }
+  function subNumber() {
+    return (
+      setNumber(number - 1)
+    )
+  }
+
+
   return (
     <div className="App">
-      <div class-Name="container">
+      <div className="container">
         <h1>Tomato Counter</h1>
         <div className="tomato-container">
+          <h1>{number}</h1>
           <div className="btn-container">
-            <button>+</button>
-            <button>-</button>
+            <button onClick={addNumber} >+</button>
+            <button onClick={subNumber} >-</button>
           </div>
         </div>
+        <div className ="rendered-tomatoes"></div>
       </div>
     </div>
   );
